c673967bfb2e93a7833ab58ea9ffe676a95e7198
sacros <sacros@Shubhams-MacBook-Air.local>
2018-05-28 01:35:24 +0530

    minor fixes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Users = artifacts.require("./Users.sol");
+var CertChain = artifacts.require("./CertChain.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Users);
+  deployer.deploy(CertChain);
 };
--- truffle.js
+++ truffle.js
@@ -2,9 +2,9 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 8545,
-      gas: 500000,
-      network_id: "*" // Match any network id
+      port: 7545,
+      // gas: 500000,
+      network_id: "5777" // Match any network id
     }
   }
 };
